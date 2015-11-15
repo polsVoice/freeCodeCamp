3028755f0975bc059fa88204ae080df997937f55
Brent Bartlett <brent.bartlett1@gmail.com>
2015-11-14 21:14:37 -0800

    all user stories completed

---
+++ zipline_-_personal_portfolio/script.js
@@ -0,0 +1,10 @@
+var buttons = {
+    init: function(){
+        var urls = [
+            "https://twitter.com/BrentBartlettBC",
+            "https://github.com/polsVoice",
+            
+        ]
+    }
+};
+buttons.init();
