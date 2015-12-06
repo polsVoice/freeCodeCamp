67971b4b175fb65c185dbf986e1786491d933fc9
Brent Bartlett <brent.bartlett1@gmail.com>
2015-12-05 22:29:30 -0800

    random functions working

--- zipline_-_random_quote_generator/test/script.js
+++ zipline_-_random_quote_generator/test/script.js
@@ -1,18 +1,28 @@
 var gen = {
-    getQuotes: function( file, callback ){
-        //~ return $.get( file, function( data ){
-            //~ var quotes = data.split( /\n\./ );
-            //~ return quotes;
-        //~ } );
-        $.ajax({
+    regex: /\n\./,
+    file: "quotes.txt",
+    getQuotes: function(file, callback, regex){
+        return $.ajax({
             type: "GET",
             url: file,
             success: function(data){
-                var response = data.split( /\n\./ );
-                //~ console.log(response);
+                var response = data.split(regex);
                 callback.call(this, response);
+            },
+            fail: function(xhr, stat, err){
+                console.log(err);
             }
-        });
+        
+        // getQuotes() must return a promise in order to
+        // use $.when().done()
+        }).promise();
+    },
+    randomNumber: function(min,max){
+        return Math.floor(Math.random()*(max-min+1)+min);
+    },
+    returnRandomElm: function(arr){
+        var randNum = gen.randomNumber(0, arr.length-1);
+        return arr[randNum];
     },
     init: function(){
     }
--- zipline_-_random_quote_generator/test/script_spec.js
+++ zipline_-_random_quote_generator/test/script_spec.js
@@ -1,8 +1,31 @@
 describe("Reading infile", function(){
     it("returns file contents as an array", function(done){
         gen.getQuotes("quotes.txt", function(data){
+            
+            // This is how Jasmine tests for type.
+            // Use toEqual() only; toBe() does not work
             expect(data).toEqual(jasmine.any(Array));
             done();
-        });
+        }, gen.regex);
+    });
+});
+describe("Random number", function(){
+    it("returns a number", function(){
+        expect(gen.randomNumber(0, 12)).toEqual(jasmine.any(Number));
+    });
+});
+describe("Random quote", function(){
+    var arr = [ "Laugh and the world laughs with you, snore and you \
+        sleep alone. -- Anthony Burgess",
+        "A successful man is one who makes more money than his\
+         wife can spend. A successful woman is one who can\
+         find such a man. -- Lana Turner",
+        "Roses are red, violets are blue, I'm schizophrenic,\
+         and so am I.\
+         -- Oscar Levant"
+    ];
+    it("returns a random quote", function(done){
+        expect(gen.returnRandomElm(arr)).toEqual(jasmine.any(String));
+        done();
     });
 });
