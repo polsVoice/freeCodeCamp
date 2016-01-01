688cde8b7aa528a26d39e8a79da3ba6128224b72
Brent Bartlett <brent.bartlett1@gmail.com>
2016-01-01 11:38:49 -0800

    quote inserted into Tweet

--- zipline_-_random_quote_generator/script.js
+++ zipline_-_random_quote_generator/script.js
@@ -1,3 +1,25 @@
+/* Twitter asynchronous loading */
+
+(function(){
+    window.twttr = (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0],
+        t = window.twttr || {};
+        if (d.getElementById(id)) return t;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs);
+
+        t._e = [];
+        t.ready = function(f) {
+            t._e.push(f);
+        };
+     
+      return t;
+    }(document, "script", "twitter-wjs"));
+})();
+
+
 var gen = {
     regex: /\n\./,
     file: "quotes.txt",
@@ -22,15 +44,22 @@ var gen = {
         return arr[randNum];
     },
     init: function(){
+        $("#twitterButton").attr("data-text", "YOLO");
         $("#theButton").on("click", function(){
             gen.getQuotes(gen.file, function(data){
-                var randQuote = gen.returnRandomElm(data);
+                var randQuote = gen.returnRandomElm(data),
+                    formattedQuote;
                 randQuote = randQuote.split( /[\.\n]*--/ );
                 
                 // If it's attributed, format the attribution;
                 // if not, leave it blank
                 randQuote[1] = randQuote[1] ? "&ndash; " + randQuote[1] : "";
-                $("#quote").html("&ldquo;" + randQuote[0] + '&rdquo;<br/>' + randQuote[1]);
+                formattedQuote = "&ldquo;" + randQuote[0] + '&rdquo;<br/>' + randQuote[1];
+                $("#quote").html(formattedQuote);
+                twttr.widgets.createShareButton(
+                    'https://dev.twitter.com/',
+                    document.getElementById('container'),
+                    {text: formattedQuote});
             }, gen.regex);
         });
     }
