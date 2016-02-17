758c58503d54ed69ffb12a1215602530e3a06887
Brent Bartlett <brent.bartlett1@gmail.com>
2016-02-16 16:38:53 -0800

    toggle working

--- zipline_-_pomodoro/script.js
+++ zipline_-_pomodoro/script.js
@@ -1,18 +1,45 @@
-var pd = {
-    startSec: "00",
-    startMin: "25",
-    get seconds() {return Math.floor((this.seconds/1000)%60);},
-    get start() {return this.startMin + ":" + this.startSec;},
-    time: function(){
-    },
-    init: function(){
-        var timer = document.getElementById("timer");
-        var foo = "bar";
-        timer.addEventListener("click", pd.time, false);
-        timer.textContent = pd.start;
-        setInterval(function(){
-            console.log("YO");
-        }, 1000);
-    }
+jQuery.fn.toggleFunc = function( func1, func2 ){
+	return this.each( function(){
+		$( this ).click( function(){
+			var counter = $( this ).data( "counter" ) || 1;
+			switch( counter ){
+				case 1:
+					func1();
+					break;
+				case 2:
+					func2();
+					break;
+				default:
+					break;
+			}
+			counter++;
+			if ( counter > 2 ){
+				counter = 1;
+			}
+			$( this ).data( "counter", counter );
+		} );
+	});
 };
+var pd = function(){
+    var startSec = "00",
+        startMin = "25",
+        running = false;
+        
+    function setTime(min, sec){
+        return min + ":" + sec;
+    }
+    return {
+        updateClock: function(){
+            $(this).text(setTime(startMin, startSec));
+        },
+        init: function(){
+            startSec = 60;
+            $("#clock").toggleFunc(function(){
+                console.log("Hey");
+            }, function(){
+                console.log("Volgein");
+            });
+        }
+    }
+}();
 pd.init();
