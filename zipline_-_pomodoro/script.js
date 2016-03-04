5aa98f3618c996af67995023de61bd6f7d0e403b
Brent Bartlett <brent.bartlett1@gmail.com>
2016-03-03 19:37:56 -0800

    added startClock()

--- zipline_-_pomodoro/script.js
+++ zipline_-_pomodoro/script.js
@@ -1,8 +1,8 @@
-jQuery.fn.toggleFunc = function( func1, func2 ){
-	return this.each( function(){
-		$( this ).click( function(){
-			var counter = $( this ).data( "counter" ) || 1;
-			switch( counter ){
+jQuery.fn.toggleFunc = function(func1, func2){
+	return this.each(function(){
+		$(this).click( function(){
+			var counter = $(this).data("counter") || 1;
+			switch(counter){
 				case 1:
 					func1();
 					break;
@@ -13,11 +13,11 @@ jQuery.fn.toggleFunc = function( func1, func2 ){
 					break;
 			}
 			counter++;
-			if ( counter > 2 ){
+			if (counter > 2){
 				counter = 1;
 			}
-			$( this ).data( "counter", counter );
-		} );
+			$(this).data("counter", counter);
+		});
 	});
 };
 var pd = function(){
@@ -28,15 +28,32 @@ var pd = function(){
     function setTime(min, sec){
         return min + ":" + sec;
     }
+    function startClock(){
+        $("#timer").text(setTime(startMin, startSec));
+        setInterval(function(){
+            $("#timer").text(setTime(startMin, startSec));
+            startSec--;
+            if (+startSec < 10){
+                startSec = "0" + startSec;
+            }
+            if (startSec === "00"){
+                setTimeout(function(){
+                    startsec = "59";
+                    startMin--;
+                    $("#timer").text(setTime(startMin, startSec));
+                }, 1000);
+            }
+            console.log(startMin, startSec);
+        }, 1000);
+    }
     return {
         updateClock: function(){
             $(this).text(setTime(startMin, startSec));
         },
         init: function(){
             startSec = 60;
-            $("#clock").toggleFunc(function(){
-                $("#timer").text(setTime(startMin, startSec));
-            }, function(){
+            startClock();
+            $("#clock").toggleFunc(startClock, function(){
                 console.log("Volgein");
             });
         }
