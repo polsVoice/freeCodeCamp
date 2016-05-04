99025f9d6640e9af10dbd6fcb5883481f53e7cd7
Brent Bartlett <brent.bartlett1@gmail.com>
2016-05-03 22:36:15 -0700

    location test added

--- weather_app/test/script-spec.js
+++ weather_app/test/script-spec.js
@@ -1,3 +1,8 @@
-describe("It can find the location", function(){
-    xit();
+describe("Geolocator", function(){
+    it("can find your longitude and latitude", function(done){
+      weather.getLocation(function(data){
+        expect(data).toBe('47.6691231, -122.3822538');
+        done();
+      });
+    });
 });
