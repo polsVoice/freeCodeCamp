4c86aacdb059dc1f22e657bcbf4ad0822bdb1163
Brent Bartlett <brent.bartlett1@gmail.com>
2016-05-31 19:48:58 -0700

    location detection works

--- weather_app/test/script-spec.js
+++ weather_app/test/script-spec.js
@@ -1,8 +1,6 @@
 describe("Geolocator", function(){
-    it("can find your longitude and latitude", function(done){
-      weather.getLocation(function(data){
-        expect(data).toBe('47.6691231, -122.3822538');
-        done();
-      });
+    it("can find your IP", function(done){
+      expect(weather.getIP()).toEqual("174.21.43.111");
+      done();
     });
 });
