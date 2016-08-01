0087ab23dbb3c8283456ee75518019e1e0edb11b
Brent Bartlett <brent.bartlett1@gmail.com>
2016-07-31 22:31:59 -0700

    weather detection working

--- weather_app/test/script-spec.js
+++ weather_app/test/script-spec.js
@@ -5,4 +5,18 @@ describe("Geolocator", function(){
            done();
        } );
     } );
+    it( "can find your city", function( done ){
+        weather.getLocation( function( data ){
+            expect( data.city ).toBe( "Seattle" );
+            done();
+        } );
+    } );
+} );
+describe( "Weather", function(){
+    it( "can retrieve the weather", function( done ){
+        weather.getWeather( function( data ){
+            expect( data.weather ).toEqual( [{"id":701,"main":"Mist","description":"mist","icon":"50d"}] );
+            done();
+        } );
+    } );
 } );
